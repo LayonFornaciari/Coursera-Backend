bd81d4dda706141f54456762594c19a8dfdab451
LayonFornaciari <156124592+LayonFornaciari@users.noreply.github.com>
2026-02-04 11:57:09 +0000

    Fixing Program.CS bug and building dotnet

--- UserManagementAPI/Program.cs
+++ UserManagementAPI/Program.cs
@@ -2,19 +2,19 @@ using UserManagementAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
- Services
+//Services
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
- DI: in-memory user store for now
+//DI: in-memory user store for now
 
 builder.Services.AddSingleton<IUserStore, InMemoryUserStore>();
 
 var app = builder.Build();
 
- Swagger for easy testing (enabled in Development by default)
+//Swagger for easy testing (enabled in Development by default)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
