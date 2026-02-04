8d256b14dc670b7f1e5d2555e5e2e2cad981e4d2
LayonFornaciari <156124592+LayonFornaciari@users.noreply.github.com>
2026-02-04 10:31:17 +0000

    Add CreateUserRequest DTO with validation

--- UserManagementAPI/Models/CreateUserRequest.cs
+++ UserManagementAPI/Models/CreateUserRequest.cs
@@ -1,1 +1,16 @@
-TODO: CreateUserRequest DTO
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.Models;
+
+public class CreateUserRequest
+
+{
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [StringLength(254)]
+    public string Email { get; set; } = string.Empty;
+}
