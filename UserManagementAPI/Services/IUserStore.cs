66ad9f4d7f9b7924ef2923af23c500facc93198d
LayonFornaciari <156124592+LayonFornaciari@users.noreply.github.com>
2026-02-04 10:31:52 +0000

    Define IUserStore abstraction

--- UserManagementAPI/Services/IUserStore.cs
+++ UserManagementAPI/Services/IUserStore.cs
@@ -1,1 +1,14 @@
+using UserManagementAPI.Models;
 
+namespace UserManagementAPI.Services;
+
+public interface IUserStore
+
+{
+    IEnumerable<User> GetAll();
+    User? Get(Guid id);
+    bool Add(User user);
+    bool Update(User user);
+    bool Delete(Guid id);
+    bool EmailExists(string email, Guid? excludeUserId = null);
+}
