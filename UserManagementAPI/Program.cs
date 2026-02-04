aba208c99d90eb4d70dea28ad2b2443da54fe6d7
LayonFornaciari <156124592+LayonFornaciari@users.noreply.github.com>
2026-02-04 10:30:35 +0000

    Wire up controllers, DI, and Swagger

--- UserManagementAPI/Program.cs
+++ UserManagementAPI/Program.cs
@@ -1,1 +1,26 @@
+using UserManagementAPI.Services;
 
+var builder = WebApplication.CreateBuilder(args);
+
+ Services
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+ DI: in-memory user store for now
+
+builder.Services.AddSingleton<IUserStore, InMemoryUserStore>();
+
+var app = builder.Build();
+
+ Swagger for easy testing (enabled in Development by default)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
